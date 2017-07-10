c19c41374f5d00ade577e655c52aef2a7b7c1372
Android63 <ajblane@gmail.com>
2017-07-10 14:47:30 +0100

    [refs #00002]Break out widgets Put widgets functionality into separate file, referenced from functions.php

--- functions.php
+++ functions.php
@@ -84,67 +84,11 @@ function nightingale_wp_content_width() {
 add_action( 'after_setup_theme', 'nightingale_wp_content_width', 0 );
 
 /**
- * Register widget area.
+ * Register widget areas.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function nightingale_wp_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'nightingale-wp' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'nightingale-wp' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
-
-	// First header widget area, located in the header. Empty by default.
-	register_sidebar( array(
-		'name' => esc_html__( 'Header Widget Area 1', 'nightingale-wp' ),
-		'id' => 'header-widget-area-1',
-		'description' => esc_html__( 'The first header widget area', 'nightingale-wp' ),
-		'before_widget' => '<div class="o-layout__item  u-4/12@lg">',
-		'after_widget' => '</div>',
-		'before_title' => '<h4 class="widget-title">',
-		'after_title' => '</h4>',
-	) );
-
-	// First footer widget area, located in the footer. Empty by default.
-	register_sidebar( array(
-			'name' => esc_html__( 'Footer Widget Area 1', 'nightingale-wp' ),
-			'id' => 'footer-widget-area-1',
-			'description' => esc_html__( 'The first footer widget area', 'nightingale-wp' ),
-			'before_widget' => '<div class="o-layout__item  u-4/12@lg">',
-			'after_widget' => '</div>',
-			'before_title' => '<h4 class="widget-title">',
-			'after_title' => '</h4>',
-	) );
-
-	// Second Footer Widget Area, located in the footer. Empty by default.
-	register_sidebar( array(
-			'name' => esc_html__( 'Footer Widget Area 2', 'nightingale-wp' ),
-			'id' => 'footer-widget-area-2',
-			'description' => esc_html__( 'The second footer widget area', 'nightingale-wp' ),
-			'before_widget' => '<div class="o-layout__item  u-4/12@lg">',
-			'after_widget' => '</div>',
-			'before_title' => '<h4 class="widget-title">',
-			'after_title' => '</h4>',
-	) );
-
-	// Third Footer Widget Area, located in the footer. Empty by default.
-	register_sidebar( array(
-			'name' => esc_html__( 'Footer Widget Area 3', 'nightingale-wp' ),
-			'id' => 'footer-widget-area-3',
-			'description' => esc_html__( 'The third footer widget area', 'nightingale-wp' ),
-			'before_widget' => '<div class="o-layout__item  u-4/12@lg">',
-			'after_widget' => '</div>',
-			'before_title' => '<h4 class="widget-title">',
-			'after_title' => '</h4>',
-	) );
-
-}
-add_action( 'widgets_init', 'nightingale_wp_widgets_init' );
+require get_template_directory() . '/inc/widgets.php';
 
 /**
  * Enqueue scripts and styles.
