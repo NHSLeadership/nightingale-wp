57673ab4b78c21e60f79317d4f0ba685389a7daa
Android63 <ajblane@gmail.com>
2017-08-11 11:11:24 +0100

    [refs #00019] Add login link
    Add login/out/register link to main menu.

--- functions.php
+++ functions.php
@@ -140,3 +140,17 @@ require get_template_directory() . '/inc/ribbons.php';
  * Main menu (using Walker Nav Menu).
  */
 require get_template_directory() . '/inc/walker-menu.php';
+
+// Add login/out/register link to main menu
+function add_login_logout_link($items, $args) {
+	if ( $args->theme_location != 'primary' ) {
+  return $items;
+  }
+	ob_start();
+	wp_loginout('index.php');
+	$loginoutlink = ob_get_contents();
+	ob_end_clean();
+	$items .= '<li>'. $loginoutlink .'</li>';
+	return $items;
+}
+add_filter('wp_nav_menu_items', 'add_login_logout_link', 10, 2);
