4341401de6269db12b2f7ac7987d635f6216d8de
Android63 <ajblane@gmail.com>
2017-08-16 15:06:07 +0100

    [refs #00019] Fix sub-menu panel layout
    Put sub-menu panel and sub-menu links into separate divs.

--- inc/walker-menu.php
+++ inc/walker-menu.php
@@ -5,19 +5,24 @@ class Walker_Nightingale_Menu extends Walker_Nav_Menu {
     
     public function start_lvl( &$output, $depth = 0, $args = array() ) {
       // start sub-menus
+      $item_output = $args->before;
       $id = $this->menuID;  // retrieve the menu ID
-      $output .= '<nav class="c-nav-primary__sub jsNavSub" id="menu-item-'.$id.'-sub" role="group" aria-label="submenu">';
-      $output .= '<div class="o-wrapper">';
-      $output .= '<div class="o-layout">';
-      $output .= '<ul class="c-nav-primary__sub-links">';
+      $item_output .= '<nav class="c-nav-primary__sub jsNavSub" id="menu-item-'.$id.'-sub" role="group" aria-label="submenu">';
+      $item_output .= '<div class="o-wrapper">';
+      $item_output .= '<div class="o-layout">';
+      $item_output .= $args->after;
+  		$output .= apply_filters( 'walker_nav_menu_start_lvl', $item_output, $depth, $args );
     }
 
     public function end_lvl( &$output, $depth = 0, $args = array() ) {
       // end sub-menus
-      $output .= '</ul>';
-      $output .= '</div><!-- .o-layout -->';
-      $output .= '</div><!-- .o-wrapper -->';
-      $output .= '</nav>';
+      $item_output = $args->before;
+      $item_output .= '</div><!-- .o-layout__item -->';
+      $item_output .= '</div><!-- .o-layout -->';
+      $item_output .= '</div><!-- .o-wrapper -->';
+      $item_output .= '</nav>';
+      $item_output .= $args->after;
+  		$output .= apply_filters( 'walker_nav_menu_end_lvl', $item_output, $depth, $args );
     }
 
     public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
@@ -26,48 +31,51 @@ class Walker_Nightingale_Menu extends Walker_Nav_Menu {
           $classes = (array) $item->classes;
       }
 
-      $active_class = '';
-      if( in_array('current-menu-item', $classes) ) {
-          $active_class = ' class="active"';
-      } else if( in_array('current-menu-parent', $classes) ) {
-          $active_class = ' class="active-parent"';
-      } else if( in_array('current-menu-ancestor', $classes) ) {
-          $active_class = ' class="active-ancestor"';
-      }
-
       $url = '';
       if( !empty( $item->url ) ) {
           $url = $item->url;
       }
+      
+      $item_output = $args->before;
 
-      $output .= '<li class="c-nav-primary__item"';
-      if ( $depth == 0 && $args->walker->has_children ) {
+      // Open sub-menu panel
+      if( in_array('subnav-header', $classes) ) {
+        $item_output .= '<div class="o-layout__item  u-6/12@lg">';
+      }
+      
+      if( $depth == 0 && $args->walker->has_children ) {
         // parent-menus
-        $output .= '><button class="c-nav-primary__link" data-expands=#menu-item-'.$item->ID.'-sub data-popup="" role="button" aria-expanded="false" aria-haspopup="true">'.$item->title.'</button>';
+        $item_output .= '<li class="c-nav-primary__item"><button class="c-nav-primary__link" data-expands=#menu-item-'.$item->ID.'-sub data-popup="" role="button" aria-expanded="false" aria-haspopup="true">'.$item->title.'</button></li><!-- .c-nav-primary__item -->';
       }
       else {
         // menus without childen (including sub-menus)
         if( in_array('subnav-header', $classes) ) {
           // if css classes include text 'subnav-header' then display sub-menu panel header
-          $output .= '><h4 class="u-margin-bottom-small">' . $item->title . '</h4>';
+          $item_output .= '<h4 class="u-margin-bottom-small">' . $item->title . '</h4>';
         } else if( in_array('subnav-description', $classes) ) {
           // if css classes include text 'subnav-description' then display sub-menu panel description
-          $output .= '><p class="u-margin-bottom-small"><small>' . $item->title . '</small></p>';
+          $item_output .= '<p class="u-margin-bottom-small"><small>' . $item->title . '</small></p>';
         } else if( in_array('subnav-button', $classes) ) {
           // if css classes include text 'subnav-button' then display sub-menu panel button
-          $output .= '><p class="u-margin-bottom-small"><a href="' . $url . '" class="c-btn  c-btn--primary  c-btn--full">'. $item->title .'</a></p>';
+          $item_output .= '<p class="u-margin-bottom-small"><a href="' . $url . '" class="c-btn  c-btn--primary  c-btn--full">'. $item->title .'</a></p>';
         } else {
           // standard menu item
-          $output .= $active_class . '><a href="' . $url . '" class="c-nav-primary__link">' . $item->title . '</a>';
+          $item_output .= '<li class = "c-nav-primary__item"><a href="' . $url . '" class = "c-nav-primary__link" >' . $item->title . '</a></li><!-- .c-nav-primary__item -->';
         }
       }
 
       $this->menuID = $item->ID;  // store the menu ID
 
-    }
+      // Close sub-menu panel and open next div
+      if( in_array('subnav-button', $classes) ) {
+        $item_output .= '</div><!-- .o-layout__item -->';
+        $item_output .= '<div class="o-layout__item  u-6/12@lg">';
+        $item_output .= '<ul class="c-nav-primary__sub-links">';
+      }
 
-    public function end_el( &$output, $item, $depth = 0, $args = array() ) {
-      $output .= '</li><!-- .c-nav-primary__item -->';
+      $item_output .= $args->after;
+  		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+
     }
 
 } // Walker_Nav_Menu
