c25fe7d2f9ffa0c614152a02a01ee78ad993af65
Android63 <ajblane@gmail.com>
2017-08-18 15:24:07 +0100

    [refs #00033] Simplify footer menu code

--- inc/custom-menus.php
+++ inc/custom-menus.php
@@ -2,16 +2,12 @@
 
 // Set up footer menu
 function register_footer_menu() {
-  register_nav_menus(
-    array(
-      'footer-menu' => __( 'Footer Menu' )
-     )
-   );
- }
- add_action( 'init', 'register_footer_menu' );
+  register_nav_menu( 'footer-menu', 'Footer Menu' );
+}
+add_action( 'init', 'register_footer_menu' );
  
- // Style footer menu list items
- function nightingale_menu_classes($classes, $item, $args) {
+// Style footer menu list items
+function nightingale_menu_classes($classes, $item, $args) {
   if($args->theme_location == 'footer-menu') {
     $classes[] = 'c-page-footer__nav-item';
   }
