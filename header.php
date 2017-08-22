b622113403c06d2dc51b7eab31a93667226064d6
Android63 <ajblane@gmail.com>
2017-08-22 12:26:08 +0100

    [refs #00019] Remove unnecessary font links

--- header.php
+++ header.php
@@ -15,10 +15,6 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="preload" href="<?php echo get_template_directory_uri(); ?>/node_modules/nightingale/assets/fonts/0811514e-6660-4043-92c1-23d0b8caaa2f.woff2" as="font" type="font/woff2" crossorigin />
-<link rel="preload" href="<?php echo get_template_directory_uri(); ?>/node_modules/nightingale/assets/fonts/8c92eb68-ce37-4962-a9f4-5b47d336bd1c.woff2" as="font" type="font/woff2" crossorigin />
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
 <?php wp_head(); ?>
 </head>
 
