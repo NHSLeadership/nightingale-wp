0f188674e7efa135570681be4c1add16f3ae4356
Android63 <ajblane@gmail.com>
2017-09-20 15:01:10 +0100

    [refs #00027] Change pagination markup
    Change pagination markup rather than using @extend.

--- index.php
+++ index.php
@@ -41,11 +41,17 @@ get_header(); ?>
 			endwhile;
 
 //			the_posts_navigation();
-			the_posts_pagination( array(
+			$pagination = get_the_posts_pagination( array(
 				'type'               => 'list',
 				'screen_reader_text' => 'Other Posts'
 			));
-
+			$pagination = str_replace("<ul class='page-numbers'>","<ul class='c-pagination'>",$pagination);
+			$pagination = str_replace("<li>","<li class='c-pagination__item'>",$pagination);
+			$pagination = str_replace("<a class='page-numbers'","<a class='c-pagination__link'",$pagination);
+			$pagination = str_replace('<a class="prev page-numbers"','<a class="c-sprite c-sprite--prev-blue"',$pagination);
+			$pagination = str_replace('<a class="next page-numbers"','<a class="c-sprite c-sprite--next-blue"',$pagination);
+			echo $pagination;
+			
 		else :
 
 			get_template_part( 'template-parts/content', 'none' );
