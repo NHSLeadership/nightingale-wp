262e2b560ca85040c6370e63d56cbe68063ce596
Android63 <ajblane@gmail.com>
2017-09-20 15:09:37 +0100

    [refs #00027] Move custom navigation to own file
    Move custom navigation function to a separate file.

--- index.php
+++ index.php
@@ -41,16 +41,7 @@ get_header(); ?>
 			endwhile;
 
 //			the_posts_navigation();
-			$pagination = get_the_posts_pagination( array(
-				'type'               => 'list',
-				'screen_reader_text' => 'Other Posts'
-			));
-			$pagination = str_replace("<ul class='page-numbers'>","<ul class='c-pagination'>",$pagination);
-			$pagination = str_replace("<li>","<li class='c-pagination__item'>",$pagination);
-			$pagination = str_replace("<a class='page-numbers'","<a class='c-pagination__link'",$pagination);
-			$pagination = str_replace('<a class="prev page-numbers"','<a class="c-sprite c-sprite--prev-blue"',$pagination);
-			$pagination = str_replace('<a class="next page-numbers"','<a class="c-sprite c-sprite--next-blue"',$pagination);
-			echo $pagination;
+			nightingale_pagination();
 			
 		else :
 
