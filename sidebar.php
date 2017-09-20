e541da1218875937b094e78dde218ace19510eac
Android63 <ajblane@gmail.com>
2017-09-20 16:08:02 +0100

    [refs #00056] Correct logic in sidebar
    Should display sidebar if it contains widgets.

--- sidebar.php
+++ sidebar.php
@@ -8,7 +8,7 @@
  */
 
  // only display sidebar if it contains widgets
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+if ( is_active_sidebar( 'sidebar-1' ) ) {
 	?>
 	<aside id="secondary" class="o-layout__item  u-4/12@lg" role="complementary">
 		<?php dynamic_sidebar( 'sidebar-1' ); ?>
