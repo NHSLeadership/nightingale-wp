33648e29102df594692046acd26759606b59cbc8
Android63 <ajblane@gmail.com>
2017-09-26 13:08:34 +0100

    [refs #00004] Various attempts to style GF
    Experiment using @extend and gform_field_cs_class to style
    Gravity Forms.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -1,29 +1,45 @@
 <?php
 /**
- * Style Gravity Forms using gform_field_css_class https://docs.gravityforms.com/gform_field_css_class/.
+ * Style Gravity Forms using gform_field_css_class https://docs.gravityforms.com/gform_field_css_class/
+ * which uses the field object https://docs.gravityforms.com/field-object/
  * Field Types are:
+ *  text
  *  textarea
  *  checkbox
  *  radio
- *  select
- *  text
  *  email
  *  number
+ *  select
  *  multiselect
  */
 add_filter( 'gform_field_css_class', 'custom_class', 10, 3 );
 function custom_class( $classes, $field, $form ) {
-    echo "Type = ".$field->type."<br/>";
-    echo "Label = ".$field->label."<br/>";
-    echo "Description = ".$field->description."<br/>";
     if ( $field->type == 'text' ) {
-        $classes .= ' c-form-input';
+        $classes .= ' ';
     }
     if ( $field->type == 'textarea' ) {
-        $classes .= ' c-form-input';
+        $classes .= ' ';
     }
-    if ( $field->type == 'label' ) {
-        $classes .= ' big';
+    if ( $field->type == 'checkbox' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'radio' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'email' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'number' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'select' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'multiselect' ) {
+        $classes .= ' ';
+    }
+    if ( $field->type == 'select' ) {
+        $classes .= ' ';
     }
     return $classes;
 }
