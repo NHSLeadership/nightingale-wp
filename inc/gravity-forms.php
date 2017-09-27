ebbeedaac0f072a9dba5bd7f4f2265b10acf7065
Android63 <ajblane@gmail.com>
2017-09-27 16:50:19 +0100

    [refs #00004] Style GF without @extend
    Use filters to add css classes instead of extending existing ones.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -1,7 +1,6 @@
 <?php
 /**
- * Style Gravity Forms using gform_field_css_class https://docs.gravityforms.com/gform_field_css_class/
- * which uses the field object https://docs.gravityforms.com/field-object/
+ * Style Gravity Forms
  * Field Types are:
  *  text
  *  textarea
@@ -12,34 +11,46 @@
  *  select
  *  multiselect
  */
-add_filter( 'gform_field_css_class', 'custom_class', 10, 3 );
-function custom_class( $classes, $field, $form ) {
+
+// Use gform_get_form_filter to style main form elements
+// See https://docs.gravityforms.com/gform_get_form_filter/
+add_filter( 'gform_get_form_filter', function ( $form_string, $form ) {
+
+   // Style ul
+   $form_string = str_replace( "class='gform_fields", "class='c-form-list gform_fields", $form_string );
+
+   // Replace h3 with h2
+   $form_string = str_replace( "<h3", "<h2", $form_string );
+   $form_string = str_replace( "/h3>", "/h2>", $form_string );
+
+   return $form_string;
+}, 10, 2 );
+
+// Use gform_field_content to style individual fields
+// See https://docs.gravityforms.com/gform_field_content/
+add_filter( 'gform_field_content', function ( $field_content, $field ) {
+
+    // Text inputs
     if ( $field->type == 'text' ) {
-        $classes .= ' ';
+      $field_content = str_replace( "type='text' value='' class='", "type='text' value='' class='c-form-input ", $field_content );
     }
-    if ( $field->type == 'textarea' ) {
-        $classes .= ' ';
+    
+    // Selects
+    if ( $field->type == 'select' ) {
+      $field_content = str_replace( "ginput_container ginput_container_select", "c-form-dropdown", $field_content );
+      $field_content = str_replace( "gfield_select", "c-form-dropdown__select  c-form-input", $field_content );
     }
-    if ( $field->type == 'checkbox' ) {
-        $classes .= ' ';
-    }
-    if ( $field->type == 'radio' ) {
-        $classes .= ' ';
-    }
+
+    // Emails
     if ( $field->type == 'email' ) {
-        $classes .= ' ';
+      $field_content = str_replace( "type='email' value='' class='", "type='email' value='' class='c-form-input ", $field_content );
     }
+    
+    // Numbers
     if ( $field->type == 'number' ) {
-        $classes .= ' ';
+      // Only change class of <input> elements, preserving all other values
+      $field_content = preg_replace("#<input(.*?)class='#i", "<input$1class='c-form-input ", $field_content);
     }
-    if ( $field->type == 'select' ) {
-        $classes .= ' ';
-    }
-    if ( $field->type == 'multiselect' ) {
-        $classes .= ' ';
-    }
-    if ( $field->type == 'select' ) {
-        $classes .= ' ';
-    }
-    return $classes;
-}
+    
+    return $field_content;
+}, 10, 2 );
