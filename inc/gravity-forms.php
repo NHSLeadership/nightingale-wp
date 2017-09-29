a9d60775d6362d658109740a2ae61f185a023523
Android63 <ajblane@gmail.com>
2017-09-29 10:52:27 +0100

    [refs #00004] Style main form and checkboxes
    Use GF filters to modify markup and add Nightingale CSS classes.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -12,20 +12,6 @@
  *  multiselect
  */
 
-// Use gform_get_form_filter to style main form elements
-// See https://docs.gravityforms.com/gform_get_form_filter/
-add_filter( 'gform_get_form_filter', function ( $form_string, $form ) {
-
-   // Style ul
-   $form_string = str_replace( "class='gform_fields", "class='c-form-list gform_fields", $form_string );
-
-   // Replace h3 with h2
-   $form_string = str_replace( "<h3", "<h2", $form_string );
-   $form_string = str_replace( "/h3>", "/h2>", $form_string );
-
-   return $form_string;
-}, 10, 2 );
-
 // Use gform_field_content to style individual fields
 // See https://docs.gravityforms.com/gform_field_content/
 add_filter( 'gform_field_content', function ( $field_content, $field ) {
@@ -34,7 +20,7 @@ add_filter( 'gform_field_content', function ( $field_content, $field ) {
     if ( $field->type == 'text' ) {
       $field_content = str_replace( "type='text' value='' class='", "type='text' value='' class='c-form-input ", $field_content );
     }
-    
+
     // Selects
     if ( $field->type == 'select' ) {
       $field_content = str_replace( "ginput_container ginput_container_select", "c-form-dropdown", $field_content );
@@ -48,9 +34,57 @@ add_filter( 'gform_field_content', function ( $field_content, $field ) {
     
     // Numbers
     if ( $field->type == 'number' ) {
-      // Only change class of <input> elements, preserving all other values
       $field_content = preg_replace("#<input(.*?)class='#i", "<input$1class='c-form-input ", $field_content);
     }
     
+    // Checkboxes
+    if ( $field->type == 'checkbox' ) {
+
+      // Replace main <label> elements with <strong>s
+      $field_content = preg_replace("#<label class=(.*?)</label>#i", "<strong class=$1</strong>", $field_content);
+
+      // Style <strong>s
+      $field_content = str_replace("<strong class='gfield_label'", "<strong class='c-form-label'", $field_content);
+      
+      // Remove <div>s
+      $field_content = preg_replace("#<div(.*?)>#i", "", $field_content);
+      $field_content = str_replace("</div>", "", $field_content);
+
+      // Remove <ul>s
+      $field_content = preg_replace("#<ul(.*?)>#i", "", $field_content);
+      $field_content = str_replace("</ul>", "", $field_content);
+
+      // Replace <label>s with <span>s
+      $field_content = preg_replace("#<label for=(.*?)>(.*?)</label>#i", "<span class='c-form-checkbox__faux'></span>$2", $field_content);
+      
+      // Replace <li> elements with <label>s
+      $field_content = str_replace("<li class='", "<label class='c-form-checkbox ", $field_content);
+      $field_content = str_replace("</li", "</label", $field_content);
+      
+      // Style <label>s
+      $field_content = str_replace('gfield_checkbox', 'c-form-checkbox', $field_content);
+
+      // Style <input>s
+      $field_content = str_replace( "type='checkbox'", "type='checkbox' class='c-form-checkbox__input'", $field_content );
+
+    }
+    
     return $field_content;
+}, 10, 2 );
+
+// Use gform_get_form_filter to style main form elements
+// See https://docs.gravityforms.com/gform_get_form_filter/
+add_filter( 'gform_get_form_filter', function ( $form_string, $form ) {
+
+   // Style <ul>
+   $form_string = str_replace( "class='gform_fields", "class='c-form-list gform_fields", $form_string );
+
+   // Replace <h3> with <h2>
+   $form_string = str_replace( "<h3", "<h2", $form_string );
+   $form_string = str_replace( "/h3>", "/h2>", $form_string );
+
+   // Replace field description divs with <small> elements
+   $form_string = preg_replace("#<div class='gfield_description'>(.*?)</div>#i", "<small>$1</small>", $form_string);
+
+   return $form_string;
 }, 10, 2 );
