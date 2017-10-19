06f744d73b6a0ea2bb49acd8b25952bd6341d44b
Android63 <ajblane@gmail.com>
2017-10-19 16:29:22 +0100

    [refs #00038] Restrict excerpts to home page
    Also add read more links.

--- functions.php
+++ functions.php
@@ -176,3 +176,11 @@ require get_template_directory() . '/inc/breadcrumbs.php';
  * Gravity Forms styling.
  */
 require get_template_directory() . '/inc/gravity-forms.php';
+
+/**
+ * Read more links.
+ */
+function new_excerpt_more( $more ) {
+    return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
