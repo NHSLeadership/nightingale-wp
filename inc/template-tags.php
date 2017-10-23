79d6a5a2a075d130fd0f0d397328d905f6b566a9
Android63 <ajblane@gmail.com>
2017-10-23 11:54:00 +0100

    [refs #00038] Remove categories & tags
    Remove links to categories and tags from posts.

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -42,20 +42,6 @@ if ( ! function_exists( 'nightingale_wp_entry_footer' ) ) :
  * Prints HTML with meta information for the categories, tags and comments.
  */
 function nightingale_wp_entry_footer() {
-	// Hide category and tag text for pages.
-	if ( 'post' === get_post_type() ) {
-		/* translators: used between list items, there is a space after the comma */
-		$categories_list = get_the_category_list( esc_html__( ', ', 'nightingale-wp' ) );
-		if ( $categories_list && nightingale_wp_categorized_blog() ) {
-			printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'nightingale-wp' ) . '</span>', $categories_list ); // WPCS: XSS OK.
-		}
-
-		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( ', ', 'nightingale-wp' ) );
-		if ( $tags_list ) {
-			printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'nightingale-wp' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-		}
-	}
 
 	if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 		echo '<span class="comments-link">';
