432ba06013c0fd2d42fda072777523670f5aafdb
Android63 <ajblane@gmail.com>
2017-11-23 15:11:20 +0000

    [refs #00038] Make content template match others
    Update content template so it matches excerpt and title only templates
    used when listing posts.

--- template-parts/content.php
+++ template-parts/content.php
@@ -17,32 +17,23 @@
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
-		
-		
+
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php nightingale_wp_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php
-		endif;
- 	
-	?></header><!-- .entry-header -->
+		endif; ?>
+	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nightingale-wp' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
 
-			// Display featured image
-			the_post_thumbnail();
-
-			/* translators: %s: Name of current post. */
-			$post_parameters = sprintf(wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'nightingale-wp' ), array( 'span' => array( 'class' => array() ) ) ), the_title( '<span class="screen-reader-text">"', '"</span>', false ));
-
-			// If excerpts are enabled in customizer, display them on home page, category page and archive pages only. Otherwise display full post
-			if ( get_theme_mod('excerpts') && ( is_home () || is_category() || is_archive() ) ) {
-				the_excerpt( $post_parameters );
-			} else {
-				the_content( $post_parameters );
-			}
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nightingale-wp' ),
 				'after'  => '</div>',
@@ -50,7 +41,7 @@
 		?>
 	</div><!-- .entry-content -->
 
-	<footer class="u-margin-bottom-small entry-footer">
+	<footer class="entry-footer">
 		<?php nightingale_wp_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
