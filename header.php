78dea14a69017152b547eaf43656382c02f61398
Android63 <ajblane@gmail.com>
2018-01-30 12:43:03 +0000

    [refs #00096] Add 'jsPage' and 'no-js' to header
    To fix JavaScript errors, ads id="jsPage" and class="no-js" to header.

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> id="jsPage" class="no-js">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
