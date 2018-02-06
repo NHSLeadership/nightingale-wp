f86d9ead7b48e15f71e6abf89b3973ae758bcbd8
Android63 <ajblane@gmail.com>
2018-02-06 10:47:43 +0000

    [refs #00103] Style full and title-only posts
    Apply nightingale styling to full post listings and listings that
    display post titles only.

--- template-parts/content-title.php
+++ template-parts/content-title.php
@@ -9,18 +9,16 @@
 
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
+<article class="c-article" id="post-<?php the_ID(); ?>">
+	<header>
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h1>', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h2 class="c-article__header"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php
+		if ( 'post' === get_post_type() ) :
 			// Display category list
 			echo the_category(' | ', get_the_ID());
 			// Display tag(s)
@@ -30,22 +28,19 @@
 					echo " | <a href='/tag/$tag->slug'>" . $tag->name . '</a>'; 
 				}
 			}
-			?>
-		</div><!-- .entry-meta -->
-		<?php
 		endif; ?>
-	</header><!-- .entry-header -->
+	</header>
 
-	<div class="entry-content">
-		<?php		
+	<div class="c-article__content">
+		<?php
 			wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'nightingale-wp' ),
 				'after'  => '</div>',
 			) );
 		?>
-	</div><!-- .entry-content -->
+	</div><!-- .c-article__content -->
 
-	<footer class="entry-footer">
+	<footer>
 		<?php nightingale_wp_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
