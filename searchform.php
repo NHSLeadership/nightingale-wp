ad474421f54e4e6df123ea383da5f5c1b4be53be
Android63 <ajblane@gmail.com>
2018-03-26 14:00:15 +0100

    [refs #00041] Search form markup
    Update markup for search form to match Nightingale.

--- searchform.php
+++ searchform.php
@@ -1,5 +1,7 @@
 <!-- Search box -->
-<form class="c-search" role="search" method="get" action="/">
-  <input type="search" class="c-search c-search__input" placeholder="Search" value="" name="s">
-  <input type="submit" class="c-search c-search__btn" alt="Search" value="">
+<form class="c-search c-page-header__search" role="search" method="get" action="/">
+	<input type="search" class="c-form-input c-search__input" placeholder="Search" value="" name="s">
+	<button class="c-search__btn">
+		<span class="c-sprite  c-sprite--magnifying-glass-rev">Search</span>
+	</button>
 </form>
