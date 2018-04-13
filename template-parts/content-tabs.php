352da947e629f142b572efa849f3fcb632a4c014
Android63 <ajblane@gmail.com>
2018-04-13 16:25:35 +0100

    [refs #00037] Check page is child or parent
    Before generating navigation tabs, make sure the page is a child (has a
    parent) or is a parent (has child pages).

--- template-parts/content-tabs.php
+++ template-parts/content-tabs.php
@@ -7,61 +7,77 @@
  * @package nightingale-wp
  */
 
- // Start unordered list
- echo '<ul class="c-tabs">';
+	// Check that page is a child (has a parent) or is a parent (has child pages)
+	$args = array(
+		'post_type' => 'page',
+		'post_parent' => $post->ID,
+		'depth' => 1,
+		'post_status' => 'publish',
+		'meta_query' => array(
+			array(
+				'key' => '_wp_page_template',
+				'value' => 'page-tabbed.php',
+			),
+		),
+	);
+	if( !empty($post->post_parent) || !empty(get_children($args)) ) {
 
-	 // Start first "Overview" link to parent page
-	 $link = '<li class="c-tabs__item"><a class="c-tabs__link';
+	 // Start unordered list
+	 echo '<ul class="c-tabs">';
 
-	 if( empty($post->post_parent) ) {
-		 // On first arrival at the parent page, the Overview link is current (and therefore inactive)
-		 // and all the other links are to child pages...
-		 $link .= ' is-current';
-		 $post_parent = $post->ID;
-	 }
-	 else {
-		 // ...but, once a child page is opened, the Overview link should point to the parent page
-		 // and all the other links to sibling pages
-		 $post_parent = $post->post_parent;
-	 }
-
-	 // Finish building and displaying Overview tab
-	 $link .= '" href="';
-	 $link .= get_permalink($post_parent);
-	 $link .= '"><span class="c-sprite  c-sprite--home  c-tabs__icon"></span><span class="c-tabs__text">';
-	 $link .= 'Overview';
-	 $link .= '</span></a></li>';
-	 echo $link;
-
-	 // Get all child/sibling pages (depending on whether this is a parent or child page) that use this tabbed page template
-	 $args = array(
-		 'post_type' => 'page',
-		 'post_parent' => $post_parent,
-		 'order' => 'ASC',
-		 'depth' => 1,
-		 'post_status' => 'publish',
-		 'meta_query' => array(
-			 array(
-				 'key' => '_wp_page_template',
-				 'value' => 'page-tabbed.php',
-			 ),
-		 ),
-	 );
-	 $my_query = new WP_Query($args);
-
-	 // Display child/sibling tabs
-	 while ( $my_query->have_posts() ) {
-		 $my_query->the_post();
+		 // Start first "Overview" link to parent page
 		 $link = '<li class="c-tabs__item"><a class="c-tabs__link';
-		 if ( is_page($post->ID) ) {
+
+		 if( empty($post->post_parent) ) {
+			 // On first arrival at the parent page, the Overview link is current (and therefore inactive)
+			 // and all the other links are to child pages...
 			 $link .= ' is-current';
+			 $post_parent = $post->ID;
 		 }
+		 else {
+			 // ...but, once a child page is opened, the Overview link should point to the parent page
+			 // and all the other links to sibling pages
+			 $post_parent = $post->post_parent;
+		 }
+
+		 // Finish building and displaying Overview tab
 		 $link .= '" href="';
-		 $link .= get_permalink($post);
+		 $link .= get_permalink($post_parent);
 		 $link .= '"><span class="c-sprite  c-sprite--home  c-tabs__icon"></span><span class="c-tabs__text">';
-		 $link .= $post->post_title;
+		 $link .= 'Overview';
 		 $link .= '</span></a></li>';
 		 echo $link;
-	 }
-	 echo '</ul>';
-	 wp_reset_query();
+
+		 // Get all child/sibling pages (depending on whether this is a parent or child page) that use this tabbed page template
+		 $args = array(
+			 'post_type' => 'page',
+			 'post_parent' => $post_parent,
+			 'order' => 'ASC',
+			 'depth' => 1,
+			 'post_status' => 'publish',
+			 'meta_query' => array(
+				 array(
+					 'key' => '_wp_page_template',
+					 'value' => 'page-tabbed.php',
+				 ),
+			 ),
+		 );
+		 $my_query = new WP_Query($args);
+
+		 // Display child/sibling tabs
+		 while ( $my_query->have_posts() ) {
+			 $my_query->the_post();
+			 $link = '<li class="c-tabs__item"><a class="c-tabs__link';
+			 if ( is_page($post->ID) ) {
+				 $link .= ' is-current';
+			 }
+			 $link .= '" href="';
+			 $link .= get_permalink($post);
+			 $link .= '"><span class="c-sprite  c-sprite--home  c-tabs__icon"></span><span class="c-tabs__text">';
+			 $link .= $post->post_title;
+			 $link .= '</span></a></li>';
+			 echo $link;
+		 }
+		 echo '</ul>';
+		 wp_reset_query();
+}
