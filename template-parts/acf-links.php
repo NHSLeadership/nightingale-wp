2ac0f7714ebf48a0e3428a1972f193096c0da9ef
Android63 <ajblane@gmail.com>
2018-05-02 15:53:15 +0100

    [refs #00127] Format homepage links
    Display links from ACF on home page in three columns.

--- template-parts/acf-links.php
+++ template-parts/acf-links.php
@@ -4,14 +4,18 @@ if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
     //Check that the ACF plugin is activated
 
 	if( have_rows('links') ):
+
+		echo '<hr class="c-divider"><div class="o-layout  o-layout--wide">';
+
 	     // loop through the rows of data
 	    while ( have_rows('links') ) : the_row();
 
-	        the_sub_field('url');
-	        the_sub_field('title');
-	        the_sub_field('description');
+					echo '<div class="o-layout__item  u-4/12@lg"><h3><a href="' . get_sub_field('url') . '">' . get_sub_field('title') . '</a></h3>';
+					echo '<p>' . get_sub_field('description') . '</p></div><!-- o-layout__item -->';
 
 	    endwhile;
+
+		echo '</div><hr class="c-divider">';
 
 	else :
 
