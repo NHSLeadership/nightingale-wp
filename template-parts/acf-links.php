8cc3392362b78f5ad5509e90e17850386404827e
Android63 <ajblane@gmail.com>
2018-05-03 16:20:24 +0100

    [refs #00127] Tidy up HTML layout
    Line breaks, indentation, etc.

--- template-parts/acf-links.php
+++ template-parts/acf-links.php
@@ -1,21 +1,23 @@
 <?php
+// Check that the ACF plugin is activated
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
-    //Check that the ACF plugin is activated
 
-	if( have_rows('links') ):
+		if( have_rows('links') ):
 
-		echo '<hr class="c-divider"><div class="o-layout  o-layout--wide">';
+				echo '<hr class="c-divider">
+				<div class="o-layout  o-layout--wide">';
 
-	     // loop through the rows of data
-	    while ( have_rows('links') ) : the_row();
+				// loop through the rows of data
+				while ( have_rows('links') ) : the_row();
+						echo '<div class="o-layout__item  u-4/12@lg">
+								<h3><a href="' . get_sub_field('url') . '">' . get_sub_field('title') . '</a></h3>
+								<p>' . get_sub_field('description') . '</p>
+						</div><!-- o-layout__item -->';
+				endwhile;
 
-					echo '<div class="o-layout__item  u-4/12@lg"><h3><a href="' . get_sub_field('url') . '">' . get_sub_field('title') . '</a></h3>';
-					echo '<p>' . get_sub_field('description') . '</p></div><!-- o-layout__item -->';
-
-	    endwhile;
-
-		echo '</div><hr class="c-divider">';
+				echo '</div>
+				<hr class="c-divider">';
 
 	else :
 
