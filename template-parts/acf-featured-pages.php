627652f0ad06a9ec8086b5a6f9dc08824b8403a7
Android63 <ajblane@gmail.com>
2018-05-09 18:49:59 +0100

    [refs #00130] Make featured pages more flexible
    Allow admins to choose from elements such as videos, images, quotes,
    text, buttons, etc.

--- template-parts/acf-featured-pages.php
+++ template-parts/acf-featured-pages.php
@@ -1,45 +1,61 @@
 <?php
-/**
-* Template part for displaying featured pages on the home page.
-*
-* Each featured page is displayed in a block consisting of two elements, one above the other.
-* Elements can be images, videos or quotes.
-* Blocks are arranged into three columns.
-*
-**/
-
 // check that the ACF plugin is activated
 include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
 
 		if( have_rows('featured_pages') ):
 
-				echo '<hr class="c-divider">
-				<div class="o-layout  o-layout--wide">';
+				echo '<hr class="c-divider">';
+				echo '<div class="o-layout  o-layout--wide">';
 
-						// loop through featured pages
-						while ( have_rows('featured_pages') ) : the_row();
-								echo '<div class="o-layout__item  u-4/12@lg">
-										<h3><a href="' . get_sub_field('page') . '">' . get_sub_field('title') . '</a></h3>';
+				// loop through the rows of data
+				while ( have_rows('featured_pages') ) : the_row();
 
-								// loop through feature block (which should contain two elements)
-								while ( have_rows('feature_block') ) : the_row();
-										// Image
-										if( get_sub_field('image') ) {
-											get_template_part( 'template-parts/acf', 'media' );
-										}
-										// Video
-										elseif ( get_sub_field('video') ) {
-											get_template_part( 'template-parts/acf', 'video' );
-										}
-										// Quote
-										while ( have_rows('quote_block') ) : the_row();
-												get_template_part( 'template-parts/acf', 'quote' );
-										endwhile;
+						echo '<div class="o-layout__item  u-4/12@lg">';
+
+						// page header with title link
+						if ( have_rows('page_header') ) :
+
+								while ( have_rows('page_header') ) : the_row();
+										echo '<h3><a href="' . get_sub_field('page_link') . '">' . get_sub_field('page_title') . '</a></h3>';
 								endwhile;
 
-								echo	'</div><!-- o-layout__item -->';
-						endwhile;
+						endif;
+
+						// page body with any combination of videos, images, quotes, etc.
+						if ( have_rows('page_body') ) :
+
+								while ( have_rows('page_body') ) : the_row();
+
+										if( get_sub_field('video') ):
+
+												get_template_part( 'template-parts/acf', 'video' );
+
+										elseif( get_sub_field('image') ):
+
+												get_template_part( 'template-parts/acf', 'media' );
+
+										elseif( get_sub_field('quote') ):
+
+												get_template_part( 'template-parts/acf', 'quote' );
+
+										elseif( get_sub_field('text') ):
+
+												get_template_part( 'template-parts/acf', 'text' );
+
+										elseif( get_sub_field('button') ):
+
+												get_template_part( 'template-parts/acf', 'button' );
+
+										endif;
+
+								endwhile;
+
+						endif;
+
+						echo '</div><!-- o-layout__item -->';
+
+				endwhile;
 
 				echo '</div><!-- o-layout -->';
 
--- template-parts/acf-video.php
+++ template-parts/acf-video.php
@@ -3,10 +3,15 @@
 // get iframe HTML
 $iframe = get_sub_field('video');
 
-// Remove hard coded dimensions from iframe and apply Nightigale styling
+// Remove hard coded dimensions from iframe and apply Nightigale class
 $iframe = str_replace ('<iframe width="640" height="360"', '<iframe class="c-media__media"', $iframe);
 
-// Display iframe
-echo $iframe;
-
+// Display video in $iframe
 ?>
+<figure class="c-media">
+
+    <div class="c-media__masthead">
+        <?php echo $iframe; ?>
+    </div>
+
+</figure>
