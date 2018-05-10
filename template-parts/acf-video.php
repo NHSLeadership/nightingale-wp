d10e8da69d982591f50fbc81a568f9bce0dfabf1
Android63 <ajblane@gmail.com>
2018-05-10 11:36:06 +0100

    [refs #00130] Allow for different video sizes
    Don't only look for 640 by 480 as admins may use other sizes.

--- template-parts/acf-video.php
+++ template-parts/acf-video.php
@@ -4,7 +4,7 @@
 $video = get_sub_field('video');
 
 // Remove hard coded dimensions from video iframe and apply Nightigale class
-$video = str_replace ('<iframe width="640" height="360"', '<iframe class="c-media__media"', $video);
+$video = preg_replace ('/<iframe width="(.*?)" height="(.*?)"/', '<iframe class="c-media__media"', $video);
 
 // Display video
 ?>
