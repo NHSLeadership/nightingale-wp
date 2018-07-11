2e5c813b8a8e7439bac894503dc208cbeb7f4e0d
Android63 <ajblane@gmail.com>
2018-07-11 10:08:11 +0100

    [refs #00181] Widget area in front page
    Replace hard-coded list of recent posts with standard sidebar in
    front page secondary content area.

--- front-page.php
+++ front-page.php
@@ -29,7 +29,6 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-	<?php get_template_part( 'template-parts/front-page', 'secondary' );?>
 	<?php get_sidebar(); ?>
 
 </div><!-- .o-layout -->
