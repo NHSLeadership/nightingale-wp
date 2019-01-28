06efa782a201865498fa8e0b9138ba7be16226dd
Android63 <ajblane@gmail.com>
2019-01-28 12:22:53 +0000

    [refs #DI-281] Extend GF save and continue expiry
    Change expiration of save and continue links from 30 days to 1 year.

--- inc/gravity-forms.php
+++ inc/gravity-forms.php
@@ -10,6 +10,9 @@
  *  number
  *  select
  *  multiselect
+ *
+ * Also extend expiration of save and continue links from 30 days to 1 year
+ *
  */
 
  // Use gform_get_form_filter to style main form elements
@@ -41,8 +44,8 @@
 
     // Replace field instruction divs with <small> elements
     $form_string = preg_replace("#<div class='instruction(.*?)>(.*?)</div>#", "<small>$2</small>", $form_string);
-    
-    // Indicate mandatory fields with "Required" rather than "*" 
+
+    // Indicate mandatory fields with "Required" rather than "*"
     $form_string = str_replace( "<span class='gfield_required'>*</span>", "<span class='gfield_required'> (Required)</span>", $form_string );
 
     // Replace main <label> elements with <strong>s
@@ -53,7 +56,7 @@
 
     // Replace checkbox and radio option <label>s with <span>s
     $form_string = preg_replace("#<label for='choice(.*?)>(.*?)</label>#i", "<span class='c-form-checkbox__faux'></span>$2", $form_string);
-    
+
     //Style the submit button
     $form_string = str_replace( "gform_button", "c-btn c-btn--submit gform_button", $form_string );
 
@@ -84,12 +87,12 @@ add_filter( 'gform_field_content', function ( $field_content, $field ) {
     if ( $field->type == 'email' ) {
       $field_content = str_replace( "type='email' value='' class='", "type='email' value='' class='c-form-input ", $field_content );
     }
-    
+
     // Numbers
     if ( $field->type == 'number' ) {
       $field_content = preg_replace("#<input(.*?)class='#", "<input$1class='c-form-input ", $field_content);
     }
-    
+
     // Checkboxes
     if ( $field->type == 'checkbox' ) {
 
@@ -101,7 +104,7 @@ add_filter( 'gform_field_content', function ( $field_content, $field ) {
       $field_content = str_replace( "type='checkbox'", "type='checkbox' class='c-form-checkbox__input'", $field_content );
 
     }
-    
+
     // Radio buttons
     if ( $field->type == 'radio' ) {
 
@@ -116,3 +119,10 @@ add_filter( 'gform_field_content', function ( $field_content, $field ) {
 
     return $field_content;
 }, 10, 2 );
+
+// Extend expiration of save and continue links from 30 days to 1 year
+add_filter( 'gform_incomplete_submissions_expiration_days', 'gwp_days', 1, 10 );
+function gwp_days( $expiration_days ) {
+    $expiration_days = 365;
+    return $expiration_days;
+}
