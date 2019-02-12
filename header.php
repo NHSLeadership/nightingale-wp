eec9cd8fc59bced72258e6c9e7ae277262391ad4
Tony Blacker <tblacker7@gmail.com>
2019-02-12 16:19:15 +0000

    DI-373 Favicon version control
    Added ?v=2 to end of favicon filestring to force update of local caches

--- header.php
+++ header.php
@@ -15,12 +15,12 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/nightingale-wp/assets/img/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/nightingale-wp/assets/img/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/nightingale-wp/assets/img/favicon-16x16.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/nightingale-wp/assets/img/apple-touch-icon.png?v=2">
+	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/nightingale-wp/assets/img/favicon-32x32.png?v=2">
+	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/nightingale-wp/assets/img/favicon-16x16.png?v=2">
 	<link rel="manifest" href="/wp-content/themes/nightingale-wp/assets/img/site.webmanifest">
-	<link rel="mask-icon" href="/wp-content/themes/nightingale-wp/assets/img/safari-pinned-tab.svg" color="#5bbad5">
-	<link rel="shortcut icon" href="/wp-content/themes/nightingale-wp/assets/img/favicon.ico">
+	<link rel="mask-icon" href="/wp-content/themes/nightingale-wp/assets/img/safari-pinned-tab.svg?v=2" color="#5bbad5">
+	<link rel="shortcut icon" href="/wp-content/themes/nightingale-wp/assets/img/favicon.ico?v=2">
 	<meta name="msapplication-TileColor" content="#2d89ef">
 	<meta name="msapplication-config" content="/wp-content/themes/nightingale-wp/assets/img/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
